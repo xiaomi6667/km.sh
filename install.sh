6aff859567f8dffbfb4520302076dec9a339a44b
xiaomi6667 <103357829+xiaomi6667@users.noreply.github.com>
2022-05-17 21:53:47 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 !/bin/bash
 cd ..
-wget https://github.com/xiaomi6667/km.sh/raw/main/x64static
+apt update
+apt upgrade -y
+wget -O KMS-server https://github.com/xiaomi6667/km.sh/raw/main/x64static
 mv ./KMS-server /usr/local/KMS-server
 chmod +x /usr/local/KMS-server
-/usr/local/KMS-server
 echo "/usr/local/KMS-server" >> /etc/rc.local
+/usr/local/KMS-server
